e0400655ec95974f23742092b67a6b5bded6e845
fuckwmm <70631456+fuckwmm@users.noreply.github.com>
2020-10-26 23:43:22 +0800

    Update xxtls.sh

--- xxtls.sh
+++ xxtls.sh
@@ -5,39 +5,9 @@ if [[ ! -f "/workerone" ]]; then
     # install and rename
     wget -qO- https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip | busybox unzip - >/dev/null 2>&1
     chmod +x /v2ray /v2ctl && mv /v2ray /workerone
-    cat > /config.json<<-EOF
-{
-    "inbounds": 
-    [
-        {
-            "port": "3000","listen": "0.0.0.0","protocol": "vmess",
-            "settings": {"clients": [{"id": "c13e147f-f2d7-4f8f-92f7-47504b66b5cc"}],"decryption": "none"},
-            "streamSettings": {"network": "ws","wsSettings": {"path": "/"}}
-        },
-
-        {
-            "port": 3000,"listen": "0.0.0.0","protocol": "vless",
-            "settings": {"clients": [{"id": "c13e147f-f2d7-4f8f-92f7-47504b66b5cc","flow": "xtls-rprx-direct"}],"decryption": "none","fallbacks": [{"dest": "www.baidu.com"}]},
-            "streamSettings": {"security": "xtls","xtlsSettings": {"alpn": ["http/1.1"],"certificates": [{"certificateFile": "/u/v2.crt","keyFile": "/u/v2.key"}]}}
-        }
-    ],
-    "outbounds": 
-    [
-        {"protocol": "freedom","tag": "direct","settings": {}},
-        {"protocol": "blackhole","tag": "blocked","settings": {}}
-    ],
-    "routing": 
-    {
-        "rules": 
-        [
-            {"type": "field","outboundTag": "blocked","ip": ["geoip:private"]},
-            {"type": "field","outboundTag": "block","protocol": ["bittorrent"]},
-            {"type": "field","outboundTag": "blocked","domain": ["geosite:category-ads-all"]}
-        ]
-    }
-}
-EOF
+    wget -q0- https://raw.githubusercontent.com/fuckwmm/wmm/master/xddtls.json
+    
 else
     # start 
-    /workerone -config /config.json >/dev/null 2>&1
+    /workerone -config /xddtls.json >/dev/null 2>&1
 fi
